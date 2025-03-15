fef30911a9980f95f78da0bd2e45b17552a2e414
Yassine Lamouadden <ylamouadden@smb.sqli.com>
2025-03-15 13:44:54 +0100

    srp

--- srp/src/main/java/com/codurance/srp/AccountService.java
+++ srp/src/main/java/com/codurance/srp/AccountService.java
@@ -1,16 +1,6 @@
 package com.codurance.srp;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.MessageFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import static java.util.stream.Collectors.toCollection;
 
 public class AccountService {
 
@@ -20,12 +10,14 @@ public class AccountService {
 
     private TransactionRepository transactionRepository;
     private Clock clock;
-    private Console console;
+    private PrinterService printerService;
 
-    public AccountService(TransactionRepository transactionRepository, Clock clock, Console console) {
+    public AccountService(TransactionRepository transactionRepository,
+                          Clock clock,
+                          PrinterService printerService) {
         this.transactionRepository = transactionRepository;
         this.clock = clock;
-        this.console = console;
+        this.printerService = printerService;
     }
 
     public void deposit(int amount) {
@@ -37,26 +29,7 @@ public class AccountService {
         transactionRepository.add(transactionWith(-amount));
     }
 
-    public void printStatement() {
-        printHeader();
-        printTransactions();
-    }
 
-
-    private void printHeader() {
-        printLine(STATEMENT_HEADER);
-    }
-
-
-    private void printTransactions() {
-        List<Transaction> transactions = transactionRepository.all();
-        final AtomicInteger balance = new AtomicInteger(0);
-        transactions.stream()
-                .map(transaction -> statementLine(transaction, balance.addAndGet(transaction.amount())))
-                .collect(toCollection(LinkedList::new))
-                .descendingIterator()
-                .forEachRemaining(this::printLine);
-    }
 
 
     private Transaction transactionWith(int amount) {
@@ -64,22 +37,9 @@ public class AccountService {
     }
 
 
-    private String statementLine(Transaction transaction, int balance) {
-        return MessageFormat.format("{0} | {1} | {2}", formatDate(transaction.date()), formatNumber(transaction.amount()), formatNumber(balance));
-    }
-
-    private String formatDate(LocalDate date) {
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        return dateFormatter.format(date);
-    }
-
-    private String formatNumber(int amount) {
-        DecimalFormat decimalFormat = new DecimalFormat(AMOUNT_FORMAT, DecimalFormatSymbols.getInstance(Locale.UK));
-        return decimalFormat.format(amount);
+    public void printStatement() {
+        printerService.printStatement();
     }
 
 
-    private void printLine(String line) {
-        console.printLine(line);
-    }
 }
---
+++ srp/src/main/java/com/codurance/srp/PrinterService.java
@@ -0,0 +1,70 @@
+package com.codurance.srp;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.stream.Collectors.toCollection;
+
+public class PrinterService {
+
+    private static final String DATE_FORMAT = "dd/MM/yyyy";
+    private static final String STATEMENT_HEADER = "DATE | AMOUNT | BALANCE";
+    private static final String AMOUNT_FORMAT = "#.00";
+
+
+    private TransactionRepository transactionRepository;
+    private Console console;
+
+    public PrinterService(TransactionRepository transactionRepository, Console console) {
+        this.transactionRepository = transactionRepository;
+        this.console = console;
+    }
+
+
+
+     void printHeader() {
+        printLine(STATEMENT_HEADER);
+    }
+
+
+     void printTransactions() {
+        List<Transaction> transactions = transactionRepository.all();
+        final AtomicInteger balance = new AtomicInteger(0);
+        transactions.stream()
+                .map(transaction -> statementLine(transaction, balance.addAndGet(transaction.amount())))
+                .collect(toCollection(LinkedList::new))
+                .descendingIterator()
+                .forEachRemaining(this::printLine);
+    }
+
+    private String statementLine(Transaction transaction, int balance) {
+        return MessageFormat.format("{0} | {1} | {2}", formatDate(transaction.date()), formatNumber(transaction.amount()), formatNumber(balance));
+    }
+
+    private String formatDate(LocalDate date) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        return dateFormatter.format(date);
+    }
+
+    private String formatNumber(int amount) {
+        DecimalFormat decimalFormat = new DecimalFormat(AMOUNT_FORMAT, DecimalFormatSymbols.getInstance(Locale.UK));
+        return decimalFormat.format(amount);
+    }
+
+
+    private void printLine(String line) {
+        console.printLine(line);
+    }
+
+    public void printStatement() {
+        printHeader();
+        printTransactions();
+    }
+}
